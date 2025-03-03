8fc9497bb58baf950bb26f801bda6e0e09b81c08
OOSad <77910065+OOSad@users.noreply.github.com>
2025-03-03 17:03:20 -0300

    Added and wired up Cancel? button

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -4,18 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ChangeScene(int SceneNumber)
     {
         SceneManager.LoadScene(SceneNumber);
