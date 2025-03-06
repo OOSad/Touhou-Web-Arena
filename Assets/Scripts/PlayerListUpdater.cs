39c139612b2b81938c63dbb0796636a250081520
OOSad <77910065+OOSad@users.noreply.github.com>
2025-03-06 06:51:02 -0300

    Created Stage Screen, added spawn points

--- Assets/Scripts/PlayerListUpdater.cs
+++ Assets/Scripts/PlayerListUpdater.cs
@@ -6,19 +6,13 @@ using UnityEngine.UI;
 
 public class PlayerListUpdater : MonoBehaviour
 {
-    public Button refreshClientsButton;
     public Bayou bayou;
     public TextMeshProUGUI playerList;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        refreshClientsButton.onClick.AddListener(TaskOnClick);
-    }
 
-    void TaskOnClick()
-    {
-        
     }
 
     // Update is called once per frame
