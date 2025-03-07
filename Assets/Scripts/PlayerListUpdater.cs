006ecbc0d0c96c179f733585780a8c2eda5be0b1
OOSad <77910065+OOSad@users.noreply.github.com>
2025-03-07 03:02:32 -0300

    Got player number to sync between client and server

--- Assets/Scripts/PlayerListUpdater.cs
+++ Assets/Scripts/PlayerListUpdater.cs
@@ -1,13 +1,16 @@
+using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using FishNet.Transporting.Bayou;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerListUpdater : MonoBehaviour
+public class PlayerListUpdater : NetworkBehaviour
 {
     public Bayou bayou;
+    
     public TextMeshProUGUI playerList;
+    public readonly SyncVar<int> playerNumber = new SyncVar<int>(); 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +21,7 @@ public class PlayerListUpdater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerList.text = bayou.NetworkManager.ServerManager.Clients.Count.ToString();
+        playerNumber.Value = bayou.NetworkManager.ServerManager.Clients.Count;
+        playerList.text = playerNumber.Value.ToString();
     }
 }
